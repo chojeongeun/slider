5a5d039a1a58dea8553763f7c9218affbba64192
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-08-14 23:05:59 +0900

    슬라이더 기능 구현

---
+++ main.js
@@ -0,0 +1,16 @@
+const slider = document.querySelector('#slider');
+const panel = slider.querySelector('.panel');
+const btns = slider.querySelectorAll('.btn li');
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		for (const el of btns) el.classList.remove('on');
+		btns[idx].classList.add('on');
+
+		new Anime(panel, {
+			prop: 'margin-left',
+			value: -100 * idx + '%',
+			duration: 500,
+		});
+	});
+});
